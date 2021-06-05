aa9c51ca1a96ad268bf271c8ddbc87d3df5f416b
Henrik Holst <hholst80@users.noreply.github.com>
2021-06-05 21:54:01 +0200

    Clean code

--- src/docker-state.cr
+++ src/docker-state.cr
@@ -3,51 +3,63 @@ require "json"
 require "docker"
 require "socket"
 
+LISTEN_PORT = 3000
+
 struct State
   include JSON::Serializable
-  @Dead       : Bool
-  @Error      : String
-  @ExitCode   : Int32
-  @FinishedAt : String
-  @OOMKilled  : Bool
-  @Paused     : Bool
-  @Pid        : Int32
-  @Restarting : Bool
-  @Running    : Bool
-  @StartedAt  : String
-  @Status     : String
+  @[JSON::Field(key: "Dead")]
+  getter dead        : Bool
+  @[JSON::Field(key: "Error")]
+  getter error       : String
+  @[JSON::Field(key: "ExitCode")]
+  getter exit_code   : Int32
+  @[JSON::Field(key: "FinishedAt")]
+  getter finished_at : String
+  @[JSON::Field(key: "OOMKilled")]
+  getter oom_killed  : Bool
+  @[JSON::Field(key: "Paused")]
+  getter paused      : Bool
+  @[JSON::Field(key: "Pid")]
+  getter pid         : Int32
+  @[JSON::Field(key: "Restarting")]
+  getter restarting  : Bool
+  @[JSON::Field(key: "Running")]
+  getter running     : Bool
+  @[JSON::Field(key: "StartedAt")]
+  getter started_at  : String
+  @[JSON::Field(key: "Status")]
+  getter status      : String
+  def running?
+    @exit_code == 0 && @running
+  end
 end
 
 class WebServer
   include Router
-  @client = Docker.client
-  @server : HTTP::Server | Nil
 
   def draw_routes
     get "/:id" do |context, params|
       id = params["id"]
       res = Docker.client.get "/v1.30/containers/#{id}/json"
-      context.response.status_code = res.status_code
-      if res.status_code != 200
-        context.response.status_code = 500
+      if !res.status.success?
+        context.response.status_code = res.status_code
         context.response.print res.body
         context
       else
-        my_json = JSON.parse(res.body)
-        state = State.from_json my_json["State"].as_s
-        if state.@ExitCode != 0 || !state.@Running
+        state = State.from_json res.body, root: "State"
+        unless state.running?
           context.response.status_code = 500
         end
-        context.response.print my_json["State"].to_json, "\n"
+        state.to_json context.response
+        context.response.print "\n"
         context
       end
     end
   end
 
   def run
-    @client = Docker::Client.new
     server = HTTP::Server.new(route_handler)
-    address = server.bind_tcp "0.0.0.0", 8080
+    address = server.bind_tcp "0.0.0.0", LISTEN_PORT
     puts "Listening on http://#{address}"
     server.listen
   end
